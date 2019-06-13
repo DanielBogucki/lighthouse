b79a758485a01bed3c8f1c4d4003cf83175bc5f8
Daniel Bogucki <daniel.bogucki95@gmail.com>
2019-06-13 04:04:33 +0200

    pom.xml modified

--- src/main/java/com/dbogucki/lighthouse/controllers/BulbController.java
+++ src/main/java/com/dbogucki/lighthouse/controllers/BulbController.java
@@ -15,7 +15,6 @@ import com.dbogucki.lighthouse.models.Room;
 import com.dbogucki.lighthouse.repositories.RoomsCollection;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
